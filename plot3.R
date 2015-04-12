613f18095fcb56e4c0582a12ab13f518782d09db
Colin Kegler <kegler@post.harvard.edu>
2015-04-12 11:02:53 -0400

    Committing plot 3
    Adding plot3.R and plot3.png similar to example given in assignment.

---
+++ plot3.R
@@ -0,0 +1,45 @@
+# @file plot3.R
+# @author Colin Kegler
+# 
+# plot3.R defines one method, plot3, which generates a plot of Global Active Power vs time (the 2 day period)
+# extract from Jan 1 to Jan 2nd, 2007 
+# source : https://archive.ics.uci.edu/ml/datasets/Individual+household+electric+power+consumption
+#
+#  Special note:  The file "consumption.R" must be run as a pre-cursor to using the function plot1
+
+
+plot3 <- function() {
+    
+    theColClasses = c(rep("numeric", 7), "Date")
+    
+    ## Read outcome data
+    extract <- read.table("data_extract.txt",
+                          header=TRUE,
+                          sep=";",
+                          na.strings = "?",
+                          
+                          #colClasses = theColClasses,
+                          stringsAsFactors=FALSE,
+                          
+    )
+    
+    extract$timestamp <- as.POSIXct(extract$timestamp , format="%Y-%m-%d %H:%M:%S")
+    
+    #make custom axis labels and axisMark positions
+    axisLabels = c("Thu", "Fri", "Sat")
+    axisMarks <- as.POSIXct( c("2007-02-01 00:00:00","2007-02-02 00:00:00","2007-02-03 00:00:00") , format="%Y-%m-%d %H:%M:%S")
+    
+    # generate plot and create custom axis labels, parallel to X-axis, to accompany plot
+    plot(extract$timestamp, extract$Sub_metering_1, col="black", xaxt='n', type = "l", ylab="Energy Sub Metering")
+    lines(extract$timestamp, extract$Sub_metering_2, col="red")
+    lines(extract$timestamp, extract$Sub_metering_3, col="blue")
+    axis.POSIXct(side=1,at=axisMarks,labels=axisLabels, las=2)
+    
+    #add a custom legend
+    legendLines <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+    legend('topright', names(extract)[4,5,6] , legendLines , col=c("black", "red", "blue"),lty=1)
+    
+    dev.copy(png, file = "plot3.png")  ## Copy my plot to a PNG file
+    dev.off()  ## Don't forget to close the PNG device!
+    
+}
